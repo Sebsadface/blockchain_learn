35d55efda08fa3435201e983595c650e2b2b8c91
sebsadface <sebsadface@gmail.com>
2021-11-24 16:39:20 -0800

    update

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,12 +1,10 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
-from web3 import Web3
+from scripts.helpful_scripts import deploy_mocks, get_account
 
 
 def deploy_fund_me():
     account = get_account()
     # Pass the price feed address to the FundMe contract
-
     # if on a persistent network(eg: rinkeby), use the associate address
     # otherwise, deploy mocks
     if network.show_active() != "development":
@@ -14,12 +12,8 @@ def deploy_fund_me():
             "eth_usd_price_feed"
         ]
     else:
-        print(f"The active network is {network.show_active()}")
-        print("Deploying Mocks...")
-        if len(MockV3Aggregator) <= 0:
-            MockV3Aggregator.deploy(18, Web3.toWei(2000, "ether"), {"from": account})
-            price_feed_address = MockV3Aggregator[-1].address
-            print("Mocks Deployed!")
+        deploy_mocks
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed_address,
